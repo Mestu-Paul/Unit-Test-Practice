ce01340eec4963fcab30d9c280ac0bf138ee6683
Mestu Paul <paulmestu@gmail.com>
2024-12-05 18:57:15 +0600

    stack unit test init

---
+++ MyStackApp/MyStack.cs
@@ -0,0 +1,26 @@
+﻿namespace MyStackApp
+{
+    public class MyStack<T>
+    {
+        private List<T> Items { get; set; } = new();
+        public bool IsEmpty()
+        {
+            return !Items.Any();
+        }
+        public void Push(T item)
+        {
+            Items.Add(item);
+        }
+        public int Size()
+        {
+            return Items.Count;
+        }
+
+        public T Pop()
+        {
+            var last = Items.Last();
+            Items.Remove(last);
+            return last;
+        }
+    }
+}
---
+++ MyStackAppTest/MyStackAppTest.cs
@@ -0,0 +1,45 @@
+﻿using MyStackApp;
+using NUnit.Framework;
+
+namespace MyStackAppTest
+{
+    [TestFixture]
+    public class MyStackTest
+    {
+        [Test]
+        public async Task IsEmptyTest()
+        {
+            var stack = new MyStack<int>();
+            Assert.IsTrue(!stack.IsEmpty());
+        }
+        [Test]
+        public async Task PushOneItem()
+        {
+            var stack = new MyStack<int>();
+            stack.Push(1);
+            Assert.IsFalse(stack.IsEmpty());
+            Assert.AreEqual(1, stack.Size());
+        }
+        [Test]
+        public async Task PushThreeItems()
+        {
+            var stack = new MyStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Assert.AreEqual(3, stack.Size());
+        }
+
+        [Test]
+        public async Task PushThreePopOneItem()
+        {
+            var stack = new MyStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Pop();
+            Assert.AreEqual(2, stack.Size());
+        }
+
+    }
+}
